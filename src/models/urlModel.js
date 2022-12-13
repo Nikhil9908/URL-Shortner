784a04b95248b19cb1972773ab6b0fb514b5afb6
Farzan Khan <kfarzan13@gmail.com>
2022-12-13 11:38:28 +0530

    Phase-1 Done

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -5,7 +5,8 @@ const urlSchema = new mongoose.Schema({
         type : String,
         required : true,
         unique : true,
-        trim : true
+        trim : true,
+        lowercase : true
     },
     longUrl: {
         type : String,
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,13 @@
 const { Router } = require('express')
 const router = Router()
+const urlController = require('../controller/urlController')
 
-router.get('/test', function(req, res){
-    console.log("Hello")
+router.post('/url/shorten', urlController.createShortUrl )
+
+router.get('/:urlCode', urlController.redirectShortUrl )
+
+router.all("/**", (req, res)=>{
+    return res.status(404).send({status:false, message:"your URL is wrong plese check endpoint"})
 })
 
 module.exports = router
