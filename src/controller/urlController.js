faa1fe8945bcd1882ea4692d562d1676d2fa0e5c
AbhijitRadke <radkeabhi27@gmail.com>
2022-12-13 17:36:59 +0530

    v2

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -2,55 +2,59 @@ const shortener = require('shortid')
 const validUrl = require('valid-url')
 const urlModel = require('../models/urlModel')
 
-const createShortUrl = async function(req, res){
+const createShortUrl = async function (req, res) {
     try {
         let data = req.body
         let { longUrl } = data
 
-        if(!longUrl) return res.status(400).send({ status : false , message : "longUrl is required !!!"})
+        if (!longUrl) return res.status(400).send({ status: false, message: "longUrl is required !!!" })
+        
+        if(typeof longUrl != 'string') return res.status(400).send({ status: false, message: "Long url must be a string" });
+        
+        if (!validUrl.isUri(longUrl)) return res.status(400).send({ status: false, message: "Invalid longUrl !!!" })
 
-        if (!validUrl.isUri(longUrl)) return res.status(400).send({ status : false , message : "Invalid longUrl !!!"})
+        let urlData = await urlModel.findOne({ longUrl: longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 })
 
-        let urlData = await urlModel.findOne({ longUrl : longUrl }).select({ longUrl : 1 , shortUrl : 1 , urlCode : 1 , _id : 0 })
+        if (urlData) return res.status(200).send({ status: false, message: "Short URL is already present.", data: urlData })
 
-        if(urlData) return res.status(200).send({ status : false , message : "Short URL is already present." , data : urlData })
-
-        let baseUrl = "http://localhost:3000"
-
+        let baseUrl = "http://localhost:3000/"
         let urlCode = shortener.generate()
+        let shortUrl = baseUrl + urlCode
 
-        let shortUrl = baseUrl + '/' + urlCode
-
-        let shortUrlData = await urlModel.findOne({ shortUrl : shortUrl , urlCode : urlCode })
-
-        if(shortUrlData) return res.status(400).send({ status : false , message : "Please try again, the generated shortUrl already exist for another longUrl !!!"})
+        let shortUrlData = await urlModel.findOne({ shortUrl: shortUrl, urlCode: urlCode })
+        if (shortUrlData) return res.status(400).send({ status: false, message: "Please try again, the generated shortUrl already exist for another longUrl !!!" })
 
         data.shortUrl = shortUrl
         data.urlCode = urlCode
 
         let savedData = await urlModel.create(data)
 
-        res.status(201).send({ status : true , data : savedData })
+        const resultData = {
+            longUrl: savedData.longUrl,
+            shortUrl: savedData.shortUrl,
+            urlCode: savedData.urlCode
+        }
+        return res.status(201).send({ status: true, data: resultData })
 
     } catch (err) {
-        res.status(500).send({ status : false , message : err.message })
+        return res.status(500).send({ status: false, message: err.message })
     }
 }
 
-const redirectShortUrl = async function(req, res){
+const redirectShortUrl = async function (req, res) {
     try {
 
-        let url = req.params.urlCode.toLowerCase()
+        let urlCode = req.params.urlCode.toLowerCase()
+        if (!urlCode) return res.status(400).send({ status: false, msg: "url code not present in params" })
 
-        let urlData = await urlModel.findOne({ urlCode : url })
+        let urlData = await urlModel.findOne({ urlCode: urlCode })
+        if (!urlData) return res.status(404).send({ status: false, message: "URL not found !!!" })
 
-        if(!urlData) return res.status(404).send({ status : false , message : "URL not found !!!" })
-
-        res.status(302).redirect(urlData.longUrl)
+        return res.status(302).redirect(urlData.longUrl)
 
     } catch (err) {
-        res.status(500).send({ status : false , message : err.message })
+       return res.status(500).send({ status: false, message: err.message })
     }
 }
 
-module.exports = { createShortUrl , redirectShortUrl }
+module.exports = { createShortUrl, redirectShortUrl }
